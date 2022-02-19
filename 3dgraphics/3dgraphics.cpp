d483698dcff48f5a5b4c95af56f0be7de57468e3
Felipe Apablaza <felipe.apablaza@protonmail.com>
2022-02-19 17:01:02 -0300

    added basic mesh loading and basic still lighting

--- 3dgraphics/3dgraphics.cpp
+++ 3dgraphics/3dgraphics.cpp
@@ -19,6 +19,20 @@
 #include <assimp/postprocess.h> // Post processing flags
 
 
+struct Material {
+    glm::vec4 ambient = {1.0f, 1.0f, 1.0f, 1.0f};
+    glm::vec4 diffuse = {1.0f, 1.0f, 1.0f, 1.0f};
+    glm::vec4 specular = {1.0f, 1.0f, 1.0f, 1.0f};
+};
+
+
+struct Light {
+    glm::vec3 direction = glm::normalize(glm::vec3{0.5f, -1.0f, 0.25f});
+    glm::vec4 ambient = {0.2f, 0.2f, 0.2f, 1.0f};
+    glm::vec4 diffuse = {0.8f, 0.8f, 0.8f, 0.8f};
+};
+
+
 GLuint createShader(const std::string &source, const GLenum type) {
     GLuint shader = glCreateShader(type);
 
@@ -50,6 +64,7 @@ GLuint createShaderProgram(const std::vector<GLuint> &shaders) {
 
     for (const GLuint shader : shaders) {
         assert(shader);
+        assert(glIsShader(shader));
 
         glAttachShader(program, shader);
     }
@@ -91,60 +106,255 @@ GLuint createBuffer(const GLenum target, const GLsizeiptr size, const void *data
 }
 
 
-GLuint createTriangleMeshVAO(const GLuint program) {
-    assert(program);
-
-    const std::array<glm::vec3, 3> vertices {
-        glm::vec3{0.0f, 1.0f, 0.0f},
-        glm::vec3{1.0f, -1.0f, 0.0f},
-        glm::vec3{-1.0f, -1.0f, 0.0f},
-    };
-
-    const std::array<glm::vec4, 3> colors {
-        glm::vec4{1.0f, 0.0f, 0.0f, 1.0f},
-        glm::vec4{0.0f, 1.0f, 0.0f, 1.0f},
-        glm::vec4{0.0f, 0.0f, 1.0f, 1.0f},
-    };
-
-    const GLuint coordBuffer = createBuffer(
-        GL_ARRAY_BUFFER, 
-        sizeof(glm::vec3) * vertices.size(), 
-        vertices.data(), GL_STATIC_DRAW
-    );
-
-    const GLuint colorBuffer = createBuffer(
-        GL_ARRAY_BUFFER, 
-        sizeof(glm::vec4) * colors.size(), 
-        colors.data(), GL_STATIC_DRAW
-    );
-
-    assert(coordBuffer);
+template<class ArrayLike>
+GLuint createBuffer(const GLenum target, const ArrayLike &values, GLenum usage) {
+    using T = typename ArrayLike::value_type;
     
+    return createBuffer(target, sizeof(T) * values.size(), values.data(), usage);
+}
+
+
+Material createMaterial(const aiMaterial *aimaterial) {
+    if (!aimaterial) {
+        return {};
+    }
+    
+    Material material;
+    
+    // extract material colors
+    aiColor3D colorAmbient, colorDiffuse, colorSpecular, colorEmissive;
+
+    aimaterial->Get(AI_MATKEY_COLOR_AMBIENT, colorAmbient);
+    aimaterial->Get(AI_MATKEY_COLOR_DIFFUSE, colorDiffuse);
+    aimaterial->Get(AI_MATKEY_COLOR_SPECULAR, colorSpecular);
+    // aimaterial->Get(AI_MATKEY_COLOR_EMISSIVE, colorEmissive);
+    
+    material.ambient = glm::vec4{colorAmbient.r, colorAmbient.g, colorAmbient.b, 1.0f};
+    material.diffuse = glm::vec4{colorDiffuse.r, colorDiffuse.g, colorDiffuse.b, 1.0f};
+    material.specular = glm::vec4{colorSpecular.r, colorSpecular.g, colorSpecular.b, 1.0f};
+
+    // extract material textures
+    aiString diffuseTextureFilePath, specularTextureFilePath, heightTextureFilePath;
+    aimaterial->GetTexture(aiTextureType_DIFFUSE, 0, &diffuseTextureFilePath);
+    aimaterial->GetTexture(aiTextureType_SPECULAR, 0, &specularTextureFilePath);
+    aimaterial->GetTexture(aiTextureType_HEIGHT, 0, &heightTextureFilePath);
+    
+    // TODO: perform texture loader via a texture manager
+    
+    return material;
+}
+
+std::vector<Material> createMaterialArray(const aiScene *aiscene) {
+    std::vector<Material> materials;
+    
+    materials.resize(aiscene->mNumMaterials);
+    
+    for (unsigned int i=0; i<aiscene->mNumMaterials; i++) {
+        materials[i] = createMaterial(aiscene->mMaterials[i]);
+    }
+    
+    return materials;
+}
+
+
+struct ShaderLocationMap {
+    GLint coord = -1;
+    GLint normal = -1;
+    
+    GLint uModel = -1;
+    GLint uView = -1;
+    GLint uProj = -1;
+    
+    GLint uMaterialAmbient = -1;
+    GLint uMaterialDiffuse = -1;
+    GLint uMaterialSpecular = -1;
+    
+    GLint uLightAmbient = -1;
+    GLint uLightDirection = -1;
+    GLint uLightDiffuse = -1;
+};
+
+
+ShaderLocationMap createShaderLocationMap(const GLuint program) {
+    assert(program);
+    assert(glIsProgram(program));
+    
+    ShaderLocationMap location;
+    
+    location.coord = glGetAttribLocation(program, "vertCoord");
+    location.normal = glGetAttribLocation(program, "vertNormal");
+    
+    location.uModel = glGetUniformLocation(program, "uModel");
+    location.uView = glGetUniformLocation(program, "uView");
+    location.uProj = glGetUniformLocation(program, "uProj");
+    
+    location.uMaterialAmbient = glGetUniformLocation(program, "uMaterialAmbient");
+    location.uMaterialDiffuse = glGetUniformLocation(program, "uMaterialDiffuse");
+    location.uMaterialSpecular = glGetUniformLocation(program, "uMaterialSpecular");
+    
+    location.uLightAmbient = glGetUniformLocation(program, "uLightAmbient");
+    location.uLightDirection = glGetUniformLocation(program, "uLightDirection");
+    location.uLightDiffuse = glGetUniformLocation(program, "uLightDiffuse");
+    
+    return location;
+}
+
+
+struct Mesh {
     GLuint vao = 0;
+    GLenum primitiveType = GL_TRIANGLES;
+    bool indexed = false;
+    unsigned int count = 0;
+    GLenum indexDataType = GL_UNSIGNED_INT;
+    
+    int material = -1;
+    
+    Mesh() {}
+    
+    bool empty() const {
+        return vao == 0;
+    }
+};
 
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
 
-    const GLint vertCoord = glGetAttribLocation(program, "vertCoord");
-    assert(vertCoord >= 0);
-
-    const GLint vertColor = glGetAttribLocation(program, "vertColor");
-    assert(vertColor >= 0);
-
-    glEnableVertexAttribArray(vertCoord);
+Mesh createMeshVAO(const ShaderLocationMap &location, const aiMesh *mesh) {
+    if (! mesh) {
+        return {};
+    }
+    
+    Mesh meshVAO;
+    
+    meshVAO.material = mesh->mMaterialIndex;
+    
+    GLuint coordBuffer = createBuffer(GL_ARRAY_BUFFER, mesh->mNumVertices * sizeof(aiVector3D), mesh->mVertices, GL_STATIC_DRAW);
+    
+    GLuint normalBuffer = 0;
+    if (mesh->HasNormals()) {
+        normalBuffer = createBuffer(GL_ARRAY_BUFFER, mesh->mNumVertices * sizeof(aiVector3D), mesh->mNormals, GL_STATIC_DRAW);
+    }
+    
+    GLuint indexBuffer = 0;
+    if (mesh->HasFaces()) {
+        // perform a little post-processing for the mesh indexing
+        // merge all triangle-shaped faces into a single index array
+        std::vector<unsigned int> indices;
+        
+        for (unsigned int j=0; j<mesh->mNumFaces; j++) {
+            const aiFace &face = mesh->mFaces[j];
+            
+            assert("This function requires the aiTriangulate postprocessing flag" && face.mNumIndices == 3);
+            
+            indices.push_back(face.mIndices[0]);
+            indices.push_back(face.mIndices[1]);
+            indices.push_back(face.mIndices[2]);
+        }
+        
+        indexBuffer = createBuffer(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW);
+        
+        meshVAO.indexDataType = GL_UNSIGNED_INT;
+        meshVAO.indexed = true;
+        meshVAO.count = indices.size();
+        meshVAO.primitiveType = GL_TRIANGLES;
+    } else {
+        meshVAO.indexDataType = GL_UNSIGNED_INT;
+        meshVAO.indexed = false;
+        meshVAO.count = mesh->mNumVertices;
+        meshVAO.primitiveType = GL_TRIANGLES;
+    }
+    
+    glGenVertexArrays(1, &meshVAO.vao);
+    glBindVertexArray(meshVAO.vao);
+    
+    glEnableVertexAttribArray(location.coord);
     glBindBuffer(GL_ARRAY_BUFFER, coordBuffer);
-    glVertexAttribPointer(vertCoord, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+    glVertexAttribPointer(location.coord, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-    glEnableVertexAttribArray(vertColor);
-    glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
-    glVertexAttribPointer(vertColor, 4, GL_FLOAT, GL_FALSE, 0, nullptr);
-
+    if (normalBuffer) {
+        assert(location.normal >= 0);
+        
+        glEnableVertexAttribArray(location.normal);
+        glBindBuffer(GL_ARRAY_BUFFER, normalBuffer);
+        glVertexAttribPointer(location.normal, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+    }
+    
+    if (indexBuffer) {
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    }
+    
     glBindVertexArray(0);
 
     assert(glGetError() == GL_NO_ERROR);
-
-    return vao;
+    
+    return meshVAO;
 }
+
+
+std::vector<Mesh> createMeshArray(const ShaderLocationMap &location, const aiScene *aiscene) {
+    if (!aiscene) {
+        return {};
+    }
+    
+    std::vector<Mesh> meshes;
+    meshes.resize(aiscene->mNumMeshes);
+    
+    for (unsigned i = 0; i < aiscene->mNumMeshes; i++) {
+        meshes[i] = createMeshVAO(location, aiscene->mMeshes[i]);
+    }
+    
+    return meshes;
+}
+
+
+//GLuint createTriangleMeshVAO(const GLuint program) {
+//    assert(program);
+//
+//    const std::vector<glm::vec3> vertices {
+//        glm::vec3{0.0f, 1.0f, 0.0f},
+//        glm::vec3{1.0f, -1.0f, 0.0f},
+//        glm::vec3{-1.0f, -1.0f, 0.0f},
+//    };
+//
+//    const std::vector<glm::vec4> colors {
+//        glm::vec4{1.0f, 0.0f, 0.0f, 1.0f},
+//        glm::vec4{0.0f, 1.0f, 0.0f, 1.0f},
+//        glm::vec4{0.0f, 0.0f, 1.0f, 1.0f},
+//    };
+//
+//    const GLuint coordBuffer = createBuffer(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
+//
+//    const GLuint colorBuffer = createBuffer(
+//        GL_ARRAY_BUFFER,
+//        sizeof(glm::vec4) * colors.size(),
+//        colors.data(), GL_STATIC_DRAW
+//    );
+//
+//    assert(coordBuffer);
+//
+//    GLuint vao = 0;
+//
+//    glGenVertexArrays(1, &vao);
+//    glBindVertexArray(vao);
+//
+//    const GLint vertCoord = glGetAttribLocation(program, "vertCoord");
+//    assert(vertCoord >= 0);
+//
+//    const GLint vertColor = glGetAttribLocation(program, "vertColor");
+//    assert(vertColor >= 0);
+//
+//    glEnableVertexAttribArray(vertCoord);
+//    glBindBuffer(GL_ARRAY_BUFFER, coordBuffer);
+//    glVertexAttribPointer(vertCoord, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+//
+//    glEnableVertexAttribArray(vertColor);
+//    glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+//    glVertexAttribPointer(vertColor, 4, GL_FLOAT, GL_FALSE, 0, nullptr);
+//
+//    glBindVertexArray(0);
+//
+//    assert(glGetError() == GL_NO_ERROR);
+//
+//    return vao;
+//}
 
 
 std::string loadTextFile(const std::string &file) {
@@ -170,7 +380,7 @@ std::string loadTextFile(const std::string &file) {
 
 
 GLuint createProgram(const std::string &vertFile, const std::string &fragFile) {
-    const std::vector<GLuint> shaders = {
+    const std::vector<GLuint> shaders {
         createShader(loadTextFile(vertFile), GL_VERTEX_SHADER), 
         createShader(loadTextFile(fragFile), GL_FRAGMENT_SHADER)
     };
@@ -187,8 +397,8 @@ int main(int argc, char **argv) {
     // propably to request more postprocessing than we do in this example.
     const auto flags =  aiProcess_Triangulate |
                         aiProcess_JoinIdenticalVertices |
-                        aiProcess_CalcTangentSpace |
-                        aiProcess_SortByPType | 
+                        // aiProcess_CalcTangentSpace |
+                        // aiProcess_SortByPType |
                         aiProcess_ValidateDataStructure;
 
     const aiScene *scene = importer.ReadFile(argv[1], flags);
@@ -209,6 +419,11 @@ int main(int argc, char **argv) {
     for (int i = 0; i<scene->mNumMeshes; i++) {
         std::cout << "Mesh: " << scene->mMeshes[i]->mName.C_Str() << std::endl;
     }
+    
+    if (scene->mNumMeshes <= 0) {
+        std::cout << "The object doesn't have any meshes" << std::endl;
+        return EXIT_FAILURE;
+    }
 
     glfwInit();
 
@@ -217,18 +432,29 @@ int main(int argc, char **argv) {
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+    glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_NATIVE_CONTEXT_API);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE);
-    glfwWindowHint(GLFW_DEPTH_BITS, 16);
+    glfwWindowHint(GLFW_DEPTH_BITS, 24);
     glfwWindowHint(GLFW_RED_BITS, mode->redBits);
     glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
     glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
     glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
     
-    GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "3dgraphics", monitor, nullptr);
-
+    GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "3dgraphics", nullptr, nullptr);
+    
     if (!window) {
         std::cout << "Cant open a Window" << std::endl;
+        
+        const char description[1024] = {};
+        const char *desc = &description[0];
+        
+        glfwGetError(&desc);
+        
+        std::cout << description << std::endl;
+
         return EXIT_FAILURE;
     }
 
@@ -246,33 +472,20 @@ int main(int argc, char **argv) {
 
     const GLuint program = createProgram("gouraud.vert", "gouraud.frag");
     assert(program);
+    
+    const ShaderLocationMap location = createShaderLocationMap(program);
+    const std::vector<Mesh> meshes = createMeshArray(location, scene);
 
-    const GLint uProj = glGetUniformLocation(program, "uProj");
-    assert(uProj >= 0);
-
-    const GLint uView = glGetUniformLocation(program, "uView");
-    assert(uView >= 0);
-
-    const GLint uModel = glGetUniformLocation(program, "uModel");
-    assert(uModel >= 0);
-
-    const GLuint triangleMeshVAO = createTriangleMeshVAO(program);
-
+    const std::vector<Material> materials = createMaterialArray(scene);
+    const Light light;
+    
     bool running = true;
 
-    const glm::mat4 proj = glm::perspective(
-        45.0f, 
-        static_cast<float>(windowWidth) / static_cast<float>(windowHeight), 
-        0.1f, 
-        100.0f);
-
-    const glm::mat4 view = glm::lookAt(
-        glm::vec3{0.0f, 0.0f, 10.0f}, 
-        glm::vec3{0.0f, 0.0f, 0.0f},
-        glm::vec3{0.0f, 1.0f, 0.0f});
-
-    const glm::mat4 model = glm::identity<glm::mat4>();
-
+    glm::vec3 playerPosition = {0.0f, 0.0f, 10.0f};
+    float angle = 0.0f;
+    
+    
+    
     while (running) {
         glfwPollEvents();
 
@@ -280,17 +493,74 @@ int main(int argc, char **argv) {
             running = false;
         }
 
+        if (glfwGetKey(window, GLFW_KEY_LEFT)) {
+            angle += 0.02f;
+        }
+        else if (glfwGetKey(window, GLFW_KEY_RIGHT)) {
+            angle -= 0.02f;
+        }
+        
+        // compute player direction
+        glm::mat4 rotationY = glm::identity<glm::mat4>();
+        rotationY = glm::rotate(rotationY, angle, glm::vec3{0.0f, 1.0f, 0.0f});
+        
+        const glm::vec3 playerDirection = rotationY * glm::vec4{0.0f, 0.0f, -1.0f, 0.0f};
+
+        // compute player movement
+        if (glfwGetKey(window, GLFW_KEY_UP)) {
+            playerPosition += 0.075f * playerDirection;
+        }
+        else if (glfwGetKey(window, GLFW_KEY_DOWN)) {
+            playerPosition -= 0.075f * playerDirection;
+        }
+        
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        glEnable(GL_DEPTH_TEST);
+        
         glUseProgram(program);
-        glUniformMatrix4fv(uProj, 1, GL_FALSE, glm::value_ptr(proj));
-        glUniformMatrix4fv(uView, 1, GL_FALSE, glm::value_ptr(view));
-        glUniformMatrix4fv(uModel, 1, GL_FALSE, glm::value_ptr(model));
 
-        glBindVertexArray(triangleMeshVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // setup transformation matrices
+        const glm::mat4 proj = glm::perspective(
+            45.0f,
+            static_cast<float>(windowWidth) / static_cast<float>(windowHeight),
+            0.1f,
+            100.0f);
 
+        const glm::mat4 view = glm::lookAt(
+            playerPosition,
+            playerPosition + playerDirection,
+            glm::vec3{0.0f, 1.0f, 0.0f});
+
+        const glm::mat4 model = glm::identity<glm::mat4>();
+        
+        glUniformMatrix4fv(location.uProj, 1, GL_FALSE, glm::value_ptr(proj));
+        glUniformMatrix4fv(location.uView, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(location.uModel, 1, GL_FALSE, glm::value_ptr(model));
+        
+        // setup lighting
+        glUniform3fv(location.uLightDirection, 1, glm::value_ptr(light.direction));
+        glUniform4fv(location.uLightAmbient, 1, glm::value_ptr(light.ambient));
+        glUniform4fv(location.uLightDiffuse, 1, glm::value_ptr(light.diffuse));
+        
+        for (const Mesh &mesh : meshes) {
+            const Material material = mesh.material >= 0 ? materials[mesh.material] : Material{};
+            
+            glUniform4fv(location.uMaterialAmbient, 1, glm::value_ptr(material.ambient));
+            glUniform4fv(location.uMaterialDiffuse, 1, glm::value_ptr(material.diffuse));
+            glUniform4fv(location.uMaterialSpecular, 1, glm::value_ptr(material.specular));
+            
+            // render the mesh
+            glBindVertexArray(mesh.vao);
+            if (mesh.indexed) {
+                glDrawElements(mesh.primitiveType, mesh.count, mesh.indexDataType, nullptr);
+            }
+            else {
+                glDrawArrays(mesh.primitiveType, 0, mesh.count);
+            }
+        }
+        
         glFlush();
         glfwSwapBuffers(window);
     }
@@ -298,6 +568,5 @@ int main(int argc, char **argv) {
     glfwDestroyWindow(window);
     glfwTerminate();
 
-    // We're done. Everything will be cleaned up by the importer destructor
     return EXIT_SUCCESS;
 };
--- 3dgraphics/gouraud.vert
+++ 3dgraphics/gouraud.vert
@@ -1,16 +1,38 @@
+
 #version 330
 
 uniform mat4 uModel;
 uniform mat4 uView;
 uniform mat4 uProj;
 
+uniform vec4 uMaterialAmbient;
+uniform vec4 uMaterialDiffuse;
+uniform vec4 uMaterialSpecular;
+
+uniform vec4 uLightAmbient;
+uniform vec3 uLightDirection;
+uniform vec4 uLightDiffuse;
+
 in vec3 vertCoord;
-in vec4 vertColor;
+in vec3 vertNormal;
 
 out vec4 fragColor;
 
 void main() {
     gl_Position = uProj * uView * uModel * vec4(vertCoord, 1.0);
 
-    fragColor = vertColor;
+    // compute modelView matrix and transform the normal with its inverse
+    mat4 modelView = uView * uModel;
+    // mat4 normalSpace = transpose(inverse(modelView));
+    // vec3 normal = (normalSpace * vec4(vertNormal, 0.0)).xyz;
+    vec3 normal = vertNormal;
+
+    // compute the diffuse factor contribution, per vertex
+    float d = abs(dot(uLightDirection, normal));
+
+    vec4 ambient = uMaterialAmbient * uLightAmbient;
+    vec4 diffuse = uMaterialDiffuse * uLightDiffuse * d;
+
+    fragColor = ambient + diffuse;
+    // fragColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
